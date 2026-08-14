93b8eb06ce5133ea5c70d60c14942e3160728c32
Albertoafa2003 <alberto.gomes@gmail.com>
2026-08-14 11:09:19 -0400

    Add files via upload

--- assets/js/cabw-numeros.js
+++ assets/js/cabw-numeros.js
@@ -71,7 +71,7 @@
     try {
       const [creditJson, rpSummary, contractsSummary] = await Promise.all([
         loadJson('assets/data/credit-current.json?v=20260814-credit-r1'),
-        loadJson('assets/data/rp-summary.json'),
+        loadJson('assets/data/rp-summary.json?v=20260814-rp-julho-r1'),
         loadJson('assets/data/contracts-summary.json')
       ]);
 
